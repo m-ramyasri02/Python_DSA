60d8d549155d8ef6e7083d467a2f5b13226e63b8
m-ramyasri02 <ramyasrimanda02@gmail.com>
2026-07-20 12:14:14 +0530

    Day 1

---
+++ r.py
@@ -0,0 +1,55 @@
+#print("\tRamya \nSri")
+'''def evenodd(n):
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+evenodd(4)'''
+'''
+dict={
+    "name":"Ramya",
+    "age":20,
+    "course":"python" 
+
+}
+dict["name"]="Ramya Sri"
+print(dict)
+print(dict.get("name"))
+print(dict.keys())
+print(dict.values())'''
+
+'''def count(*args):
+    print(type(args))
+def dicts(**kwargs):
+    print(type(kwargs))
+def default(gender,age,name="RAmya"):
+    print(age,gender,name)
+count(1,2,3,4,5)
+dicts(name="Ramya", age=20, course="python")
+default("female",20)'''
+
+#OOPs concepts
+
+#static typing and dynamic typing
+
+'''x="Ramya"
+#for i in range(1,len(x)-1):
+  #  print(x[i])
+print(x[::-1])
+print(x[1:len(x)-1])
+'''
+#infinite loop
+'''while True:
+    print("RAvi")
+'''
+x=5
+s=0
+'''
+for i in range(1,x+1):
+    s=s+i
+print(s)
+    '''
+while x>1:
+    s+=x
+    x-=1
+    print(x)
